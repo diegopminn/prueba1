6b0315f6d8c4d76fb467843b6fe2d473284f3ca7
DiMisMaster <clasesadasir2@gmail.com>
2022-09-18 19:29:48 +0200

    CONFIGURACION SIN NADA SOLO PRUEBAS DE CONTROLADOR Y PLANTILLAS

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use http\Env\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
