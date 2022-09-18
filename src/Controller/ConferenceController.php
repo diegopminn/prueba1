b8af777dd7809704ae0d46951185cc2d47741c54
DiMisMaster <clasesadasir2@gmail.com>
2022-09-18 19:27:26 +0200

    CONFIGURACION SIN NADA SOLO PRUEBAS DE CONTROLADOR Y PLANTILLAS

---
+++ src/Controller/ConferenceController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class ConferenceController extends AbstractController
+{
+    /**
+     * @Route("/hello/{name}", name="app_conference")
+     */
+    //ahora nuestra url tiene una parte fija y otra dinamica representada por una variable name
+    /*Esto de arriba es una ruta en annotacion como puse en los apuntes o en YAML o en Annotations pues aqui en anotacion el nombre de la ruta es el id*/
+    public function index(string $name): Response
+    {
+
+        return $this->render('conference/index.html.twig', [
+            'name' => $name,
+
+        ]);
+        //vamos a reemplazar el return por un metodo render de la clase que estamos heredando osea nuestro saludo
+    }
+}
